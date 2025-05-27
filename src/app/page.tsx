90145cfac0ac8885977d094fe705d077c106b36e
Baran Karakaya <thebarankarakaya@gmail.com>
2025-05-27 10:39:38 +0300

    feat: update Home component to include Header component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+"use client";
+
+import Header from "@/components/Header";
+
 export default function Home() {
   return (
     <div>
-      {/* Clean home page - ready for your content */}
+      <Header></Header>
     </div>
   );
 }
